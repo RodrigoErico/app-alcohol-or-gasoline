ffe5144c1bba0c430d5fd5b629b129be71e567be
Rodrigo Erico <rodrigoerico25@gmail.com>
2024-01-01 17:36:39 -0300

    creating variables

--- app/src/main/java/com/rodrigoerico/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/rodrigoerico/alcoolougasolina/MainActivity.kt
@@ -2,8 +2,21 @@ package com.rodrigoerico.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputAlcohol: TextInputLayout
+    private lateinit var editAlcohol: TextInputEditText
+    private lateinit var textInputGasoline: TextInputLayout
+    private lateinit var editGasoline: TextInputEditText
+
+    private lateinit var btnCalculate: Button
+    private lateinit var textResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
