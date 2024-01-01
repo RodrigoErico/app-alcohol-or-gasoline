adde44e5c2c173dc95ee67985e29653b9e53f13a
Rodrigo Erico <rodrigoerico25@gmail.com>
2024-01-01 17:44:22 -0300

    calculation result

--- app/src/main/java/com/rodrigoerico/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/rodrigoerico/alcoolougasolina/MainActivity.kt
@@ -21,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializesInterfaceComponents()
+        btnCalculate.setOnClickListener{
+            calculateBestPrice()
+        }
     }
 
     private fun initializesInterfaceComponents() {
